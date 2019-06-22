3228ecd97e4d6e9c20347a2128a6743c7848f9ed
Higor Silva Rosa <higor.s.rosa@gmail.com>
2019-06-22 03:13:00 -0300

    Camera PWA (almost finished)

--- src/app/pages/camera/camera.page.ts
+++ src/app/pages/camera/camera.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ActionSheetController, AlertController } from '@ionic/angular';
+
+CameraSource.Camera
 
 @Component({
   selector: 'app-camera',
@@ -10,21 +13,55 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class CameraPage implements OnInit {
 
   constructor(
+    private actionSheetController: ActionSheetController,
+    private alertController: AlertController,
     private domSanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
   }
 
+  askToTakePicture() {
+    this.actionSheetController.create({
+      header: 'Source?',
+      buttons: [
+        {
+          text: 'CameraSource.Camera',
+          handler: () => {
+            this.takePicture(CameraSource.Camera);
+          }
+        },
+        {
+          text: 'CameraSource.Photos',
+          handler: () => {
+            this.takePicture(CameraSource.Photos);
+          }
+        },
+        {
+          text: 'CameraSource.Prompt',
+          handler: () => {
+            this.takePicture(CameraSource.Prompt);
+          }
+        },
+      ]
+    }).then(actionSheet => actionSheet.present());
+  }
 
-  takePicture() {
+  takePicture(source: CameraSource) {
     Plugins.Camera.getPhoto({
       quality: 100,
-      allowEditing: false,
+      allowEditing: true,
       resultType: CameraResultType.DataUrl,
-      source: CameraSource.Prompt
+      source: source
     }).then(image => {
       this.domSanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+    }).catch(error => {
+      this.alertController.create({
+        header: 'Error!',
+        subHeader: "Something went wrong...",
+        message: error,
+        buttons: ['OK']
+      }).then(alert => alert.present());
     });
   }
 
